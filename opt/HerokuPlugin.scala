ec88b0d94a1b61b719884ba81d94b3fc1c5ca027
Canopy Cloud <canopy@canopy-cloud.com>
2014-03-29 17:58:56 +0000

    Update repository URLs.

--- opt/HerokuPlugin.scala
+++ opt/HerokuPlugin.scala
@@ -5,8 +5,8 @@ object HerokuPlugin extends Plugin {
   override def settings = Seq(
     externalResolvers <<= resolvers map { appResolvers =>
       Seq(Resolver.defaultLocal) ++ appResolvers ++
-      Seq(Resolver.url("heroku-sbt-typesafe") artifacts "http://s3pository.heroku.com/ivy-typesafe-releases/[organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext]",
-          "heroku-central" at "http://s3pository.heroku.com/maven-central/",
+      Seq(Resolver.url("heroku-sbt-typesafe") artifacts "http://217.115.68.26:8081/nexus/content/repositories/ivy-typesafe-releases/[organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext]",
+          "heroku-central" at "http://217.115.68.26:8081/nexus/content/repositories/maven-central/",
           "typesafe" at "http://repo.typesafe.com/typesafe/repo/")
     },
     sources in doc in Compile := List()
